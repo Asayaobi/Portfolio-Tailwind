0b33bb3e22b8dabf579be769b529bbe10efd96c4
Asaya <145620779+Asayaobi@users.noreply.github.com>
2024-05-27 18:11:57 +0200

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'treehouse': {
+          DEFAULT: '#36291E',
+          50: '#F2EDE8',
+          100: '#EAE1D9',
+          200: '#DAC9BC',
+          300: '#C9B29E',
+          400: '#B99A81',
+          500: '#A88363',
+          600: '#8F6C4F',
+          700: '#71563F',
+          800: '#543F2E',
+          900: '#36291E',
+          950: '#241B14'
+        }
+      },
+    },
   },
   plugins: [],
 }
